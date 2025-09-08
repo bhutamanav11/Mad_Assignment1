68744148ec6a9b0b2dfaf712c36dc92a29dbc4ef
Manav Bhuta <bhutamanav@gmail.com>
2025-09-08 18:57:40 +0530

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,55 @@
+plugins {
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "com.C164.ManavBhuta"
+    compileSdk = 35
+    buildFeatures {
+        buildConfig = true
+        viewBinding = true
+    }
+
+    defaultConfig {
+        applicationId = "com.C164.ManavBhuta"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        val apiKey: String = project.findProperty("GEMINI_API_KEY") as String? ?: "AIzaSyC0UpyQnm-1GH3wtta1iSwJiGeDmF1aZXM"
+        buildConfigField("String", "API_KEY", "\"${apiKey}\"")
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    implementation(libs.common)
+    implementation(libs.generativeai)
+    implementation(libs.espresso.web)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+
+    implementation("com.google.guava:guava:32.1.3-android")
+
+}
